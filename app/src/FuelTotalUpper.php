a36c6891a1cf5b2d0e8a0a1652a7fd5a63f0bf36
Anne-Laure de Boissieu <al.deboissieu@elao.com>
2019-12-03 14:58:55 +0100

    add part two

--- app/src/FuelTotalUpper.php
+++ app/src/FuelTotalUpper.php
@@ -10,13 +10,35 @@ class FuelTotalUpper
         $totalFuel = 0;
 
         foreach ($masses as $mass) {
-            $totalFuel += $this->calculateMass($mass);
+            $totalFuel += $this->calculateMassV1($mass);
         }
 
         return $totalFuel;
     }
 
-    public function calculateMass(int $mass): int
+    public function calculateMassesV2(array $masses)
+    {
+        $totalFuel = 0;
+
+        foreach ($masses as $mass) {
+            $totalFuel += $this->calculateMassV2($mass);
+        }
+
+        return $totalFuel;
+    }
+
+    public function calculateMassV2(int $mass): int
+    {
+        $fuel = $this->calculateMassV1($mass);
+
+        if ($fuel <= 0) {
+            return 0;
+        }
+
+        return $fuel + $this->calculateMassV2($fuel);
+    }
+
+    public function calculateMassV1(int $mass): int
     {
         return (floor($mass / 3) - 2);
     }
--- app/test/FuelTotalUpperTest.php
+++ app/test/FuelTotalUpperTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 
 class FuelTotalUpperTest extends TestCase
 {
-    public function testCalculateTotalMasses(): void
+    public function testCalculateTotalMassesV1(): void
     {
         $masses = include("app/data/fuel.php");
 
@@ -16,13 +16,32 @@ class FuelTotalUpperTest extends TestCase
         $this->assertEquals(3448043, $calculator->calculateMasses($masses));
     }
 
+    public function testCalculateTotalMassesV2(): void
+    {
+        $masses = include("app/data/fuel-part2.php");
+
+        $calculator = new FuelTotalUpper();
+
+        $this->assertEquals(5169198, $calculator->calculateMassesV2($masses));
+    }
+
     public function testCalculateSingleMass(): void
     {
         $calculator = new FuelTotalUpper();
 
-        $this->assertEquals(2, $calculator->calculateMass(12));
-        $this->assertEquals(2, $calculator->calculateMass(14));
-        $this->assertEquals(654, $calculator->calculateMass(1969));
-        $this->assertEquals(33583, $calculator->calculateMass(100756));
+        $this->assertEquals(2, $calculator->calculateMassV1(12));
+        $this->assertEquals(2, $calculator->calculateMassV1(14));
+        $this->assertEquals(654, $calculator->calculateMassV1(1969));
+        $this->assertEquals(33583, $calculator->calculateMassV1(100756));
+    }
+
+    public function testCalculateMassV2(): void
+    {
+        $calculator = new FuelTotalUpper();
+
+        $this->assertEquals(2, $calculator->calculateMassV2(12));
+        $this->assertEquals(2, $calculator->calculateMassV2(14));
+        $this->assertEquals(966, $calculator->calculateMassV2(1969));
+        $this->assertEquals(50346, $calculator->calculateMassV2(100756));
     }
 }
